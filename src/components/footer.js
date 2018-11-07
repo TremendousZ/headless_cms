08f13ba9a40527170dfcb4e9893f337d30e19af4
John Carlisle <jmcarlisle@live.com>
2018-11-07 14:10:44 -0800

    added footer with placeholder text for links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './footer.css';
 
 export default ()=> {
     return (
-        <div>
-            
+        <div className = 'footer-container'>
+            <div className = "footer-content">
+                <div>
+                © 2018 TechnoBuffalo LLC. All Rights Reserved.
+                </div>
+                <ul className ="footer-links">
+                    <li>Our Crew</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Privacy Policy</li>
+                    <li>Term of Use</li>
+                    <li>All Categories</li>
+                </ul>
+
+            </div>
         </div>
     )
 }
