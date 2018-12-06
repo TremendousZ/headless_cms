da157812a9056f608babf359345f655038ac6efa
John Carlisle <jmcarlisle@live.com>
2018-12-06 12:15:19 -0800

    added new featured_post component which loads images and titles

--- src/components/featured.js
+++ src/components/featured.js
@@ -1,53 +1,58 @@
 import React, {Component} from "react";
 import './featured.css';
+import FeaturedPost from './featured_post';
+import axios from 'axios';
 
 class Featured extends Component {
 
     constructor() {
         super();
         this.state = {
-          movies: []
+          posts: [],
+          response:''
         }
+        this.posts =[];
       }
     componentDidMount() {
-    //    this.getData();
+       this.getData();
       }
+      
 
-    // async getData(){
-    //     let dataURL = "https://thenerdy.com/wp-json/wp/v2/posts"
-    //     // "https://thenerdy.com/wp-json/wp/v2/media";
-    //   await fetch(dataURL)
-    //       .then(res => res.json())
-    //       .then(res => {
-    //         this.setState({
-    //           movies: res
-    //         })
-    //         console.log("RESPONSE",res);
-    //       })
-    // }
+    async getData(){
+        let dataURL = "https://thenerdy.com/wp-json/wp/v2/posts?categories=176";
+        await axios.get(dataURL,{
+            params: {
+                per_page:3,
+            }
+        }).then(response=>{
+            this.setState({response});
+            console.log("featured response", response);
+            this.createFeaturePosts(response.data);
+        });
+    }
+
+    createFeaturePosts(postArray){
+        for(let index = 0; index < postArray.length; index++){
+            let featuredPost = <FeaturedPost key={Math.random()*10} postTitle={postArray[index].title.rendered} postId={postArray[index].featured_media} />
+            this.posts.push(featuredPost);    
+        }
+        this.setState({
+            posts:this.posts,
+        })
+    }
+
+
+
+    
 
     render (){
-        
+        console.log("FEATURED RESPONSE CONSOLE LOG", this.state.response);
        
         return (
         <div className = "featured-container">
             <div className = "label">FEATURED</div>
             <div className = "featured-carousel">
-                <div className = "feature">
-                    {/* <img src={this.state.movies}/> */}
-                    <div className = "image-placeholder"></div>
-                    <span>Lorem ipsum dolor, sit amet consectetur adipisicing</span>
-                </div>
-                <div className = "feature">
-                    {/* <img /> */}
-                    <div className = "image-placeholder"></div>
-                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
-                </div>
-                <div className = "feature">
-                    {/* <img /> */}
-                    <div className = "image-placeholder"></div>
-                    <span>Lorem ipsum dolor sit amet.</span>
-                </div>
+                {this.state.posts}
             </div>
         </div>
         )
